c8bdd93c5649f8fddfd882484cad6f445b919e39
dekr0o0 <60785612+dekr0o0@users.noreply.github.com>
2026-03-04 09:29:41 +0300

    Основа

--- HELICO/Main.py
+++ HELICO/Main.py
@@ -18,35 +18,6 @@ field.generate_river(20)
 
 helico = Helico(MAP_W, MAP_H)
 
-from pynput import keyboard
-
-def on_press(key, injected):
-    try:
-        print('alphanumeric key {} pressed; it was {}'.format(
-            key.char, 'faked' if injected else 'not faked'))
-    except AttributeError:
-        print('special key {} pressed'.format(
-            key))
-
-def on_release(key, injected):
-    print('{} released; it was {}'.format(
-        key, 'faked' if injected else 'not faked'))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
-
-# ...or, in a non-blocking fashion:
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
-
 
 tick = 1
 
